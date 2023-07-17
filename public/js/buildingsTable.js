b69a259be14c6ae02bc40abda11e69f07dc5afb0
TomT-3WA <tom.timsit@3wa.io>
2023-07-17 15:40:02 +0200

    export done

--- public/js/buildingsTable.js
+++ public/js/buildingsTable.js
@@ -1,42 +1,36 @@
-var $table = $("#table");
-
 $(function () {
   $("#toolbar")
     .find("select")
     .change(function () {
-      $table.bootstrapTable("destroy").bootstrapTable({
-        exportDataType: $(this).val(),
-        exportTypes: ["json", "xml", "csv", "txt", "excel", "pdf"],
-        columns: [
-          {
-            field: "state",
-            checkbox: true,
-            visible: $(this).val() === "selected",
-          },
-          {
-            field: "id",
-            title: "ID",
-          },
-          {
-            field: "description",
-            title: "Description",
-          },
-          {
-            field: "referenceProprio",
-            title: "Référence Propriétaire",
-          },
-          {
-            field: "vendu",
-            title: "Vendu",
-          },
-        ],
-      });
+      $("#table")
+        .bootstrapTable("destroy")
+        .bootstrapTable({
+          exportDataType: $(this).val(),
+          exportTypes: ["csv", "excel"],
+          columns: [
+            {
+              field: "state",
+              checkbox: true,
+              visible: $(this).val() === "selected",
+            },
+            {
+              field: "id",
+              title: "ID",
+            },
+            {
+              field: "description",
+              title: "Description",
+            },
+            {
+              field: "referenceProprio",
+              title: "Référence Propriétaire",
+            },
+            {
+              field: "vendu",
+              title: "Vendu",
+            },
+          ],
+        });
     })
     .trigger("change");
-});
-
-$(function () {
-  $("<i class='bi bi-download'></i>").replaceAll(
-    "<i class='fa-solid fa-download'></i>"
-  );
 });
