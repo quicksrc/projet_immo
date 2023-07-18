c327efa1fbf659a6a154c8514e95b41fa84efbe7
TomT-3WA <tom.timsit@3wa.io>
2023-07-18 17:17:05 +0200

    fixing table

--- public/js/buildingsTable.js
+++ public/js/buildingsTable.js
@@ -59,33 +59,35 @@ $(function () {
   $("#toolbarContact")
     .find("select")
     .change(function () {
-      $("#tableContact").bootstrapTable("destroy").bootstrapTable({
-        exportDataType: $(this).val(),
-        exportTypes: ["json", "xml", "csv", "txt", "sql", "excel", "pdf"],
-        columns: [
-          {
-            field: "state",
-            checkbox: true,
-            visible: $(this).val() === "selected",
-          },
-          {
-            field: "id",
-            title: "ID",
-          },
-          {
-            field: "description",
-            title: "Description",
-          },
-          {
-            field: "referenceProprio",
-            title: "Référence Propriétaire",
-          },
-          {
-            field: "vendu",
-            title: "Vendu",
-          },
-        ],
-      });
+      $("#tableContact")
+        .bootstrapTable("destroy")
+        .bootstrapTable({
+          exportDataType: $(this).val(),
+          exportTypes: ["csv", "excel"],
+          columns: [
+            {
+              field: "state",
+              checkbox: true,
+              visible: $(this).val() === "selected",
+            },
+            {
+              field: "id",
+              title: "N°",
+            },
+            {
+              field: "nom",
+              title: "Nom",
+            },
+            {
+              field: "referenceProprio",
+              title: "Référence Propriétaire",
+            },
+            {
+              field: "vendu",
+              title: "Vendu",
+            },
+          ],
+        });
     })
     .trigger("change");
 });
